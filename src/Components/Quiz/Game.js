8a52758a3e3a342fb93104e607b5bfcc69dfc071
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2021-12-20 00:46:26 +0100

    Render from the server

--- src/Components/Quiz/Game.js
+++ src/Components/Quiz/Game.js
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import QuizGame from './QuizGame';
 import EmptyQuiz from './EmptyQuiz';
+import { useEffect } from "react";
 
 const Game = ({ score, setScore, currentQuiz, setCurrentQuiz, setFinished }) => {
 
+    const URL = 'https://core.dit.upm.es/api/quizzes/random10wa?token=2bca751d84825b1e6c2a'
+
     //const [quizzes, setQuizzes] = useState(require("../../assets/empty-data.json"));
-    const [quizzes, setQuizzes] = useState(require("../../assets/mock-data.json"));
+    //const [quizzes, setQuizzes] = useState(require("../../assets/mock-data.json"));
+    const [quizzes, setQuizzes] = useState([]);
+
+    useEffect(() => {
+        fetch(URL)
+            .then(res => res.json())
+            .then(json => {setQuizzes(json)})
+    }, []);
 
     return (
         <div>
