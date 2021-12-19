5d7d089c718bb69dd839fab9116b2ce81c14f76f
Marta Arcones Rodríguez <marta.arcones@gmail.com>
2021-12-19 14:18:38 +0100

    Board to functional component

--- src/Components/TicTacToe/Board.jsx
+++ src/Components/TicTacToe/Board.jsx
@@ -1,33 +1,25 @@
-import React from 'react';
 import Square from './Square';
 
-export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.boardClick = this.boardClick.bind(this);
+const Board = (props) => {
+  const boardClick = (rowNumber, columnNumber) => {
+    props.appClick(rowNumber, columnNumber);
   }
-  boardClick(rowNumber, columnNumber) {
-    this.props.appClick(rowNumber, columnNumber);
-  }
-  render() {
-    let board = this.props.values.map((rowValues, rowIndex) => {
-      let row = rowValues.map((value, columnIndex) => {
-          return (
-            <Square value={value} key={rowIndex + "-" + columnIndex} rowIndex={rowIndex}
-                    columnIndex={columnIndex} boardClick={this.boardClick}/>
-        );
-      });
-      return (
-        <div key={"row" + rowIndex}>
-          {row}
-        </div>
+
+  let board = props.values.map((rowValues, rowIndex) => {
+    let row = rowValues.map((value, columnIndex) => {
+        return (
+          <Square value={value} key={rowIndex + "-" + columnIndex} rowIndex={rowIndex}
+                  columnIndex={columnIndex} boardClick={boardClick}/>
       );
     });
-
     return (
-       <div>
-        {board}
-       </div>
+      <div key={"row" + rowIndex}>{row}</div>
     );
-  }
+  });
+
+  return (
+    <div>{board}</div>
+  )
 }
+
+export default Board
